74d964e1283a6c439a297480698cb7acb417835c
Farha-Wael <F.Wael2492@nu.edu.eg>
2025-12-07 01:43:50 +0200

    addtiton- multiplication and power operations added

--- calc.cpp
+++ calc.cpp
@@ -2,7 +2,28 @@
 #define ll long long 
 #define el '\n'
 using namespace std;
- 
+
+ll add(ll x,ll y){
+    return x+y;
+}
+
+double add(double x,double y){
+    return x+y;
+}
+
+ll mul(ll x ,ll y){
+    return x*y;
+}
+
+double mul(double x,double y){
+    return x*y;
+}
+
+double power(double base, double exp){
+    if(exp==0) return 1;
+    return power(base,exp-1);
+}
+
 int main(){
     return 0;
 }
